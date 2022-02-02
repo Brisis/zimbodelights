0d23f363b4aa1bfc68bfdc08c438690226194503
Brisis <38928449+Brisis@users.noreply.github.com>
2022-02-02 12:23:21 +0200

    final two

--- app/Mail/OrderMail.php
+++ app/Mail/OrderMail.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class OrderMail extends Mailable implements ShouldQueue
+class OrderMail extends Mailable
 {
     use Queueable, SerializesModels;
 
